73310e5f261619c34270e09f293e83f8d4548fd8
Cristian Tincu <cristian.tincu@kalon.ro>
2014-08-03 15:33:58 +0300

    Step 2. Grunt Build.

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/* globals module */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  var LIB = '../lib';
+
+  grunt.initConfig({
+
+    requirejs: {
+      compile: {
+        options: {
+
+          baseUrl: 'src/app',
+
+          paths: {
+            jquery: LIB + '/jquery',
+            underscore: LIB + '/underscore',
+            backbone: LIB + '/backbone'
+          },
+
+          name: 'main',
+          out: 'dist/app.min.js'
+
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', [ 'requirejs' ]);
+
+};
